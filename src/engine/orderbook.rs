22a618e9a2521524074c61e32a612a22b69c3ef5
Robert <rcgray306@gmail.com>
2023-05-04 23:13:33 +0100

    Fixing rusty rust code

--- src/engine/orderbook.rs
+++ src/engine/orderbook.rs
@@ -28,8 +28,7 @@ impl Orderbook {
     pub fn check_for_trades(&mut self) -> Vec<Trade> {
         let mut executed_trades: Vec<Trade> = Vec::new();
 
-        while !self.asks.is_empty() && !self.bids.is_empty() {
-            let (ask, bid) = (self.asks.peek().unwrap(), self.bids.peek().unwrap());
+        while  let (ask, bid) = (self.asks.peek().unwrap(), self.bids.peek().unwrap()) {
             // Match the optional of the merge
             match self.merge_orders(ask, bid) {
                 None => break,
