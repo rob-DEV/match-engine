05638a4a07efb70d6ac236704003a486bb5fba53
Robert <rcgray306@gmail.com>
2023-05-04 22:43:09 +0100

    added order book debug printing

--- src/engine/orderbook.rs
+++ src/engine/orderbook.rs
@@ -2,12 +2,12 @@
 
 use std::cmp::Ordering;
 use std::collections::BinaryHeap;
+use std::fmt::{Debug, Formatter};
 use crate::engine::types::{Order, Side, Trade};
 
-#[derive(Debug)]
 pub struct Orderbook {
     asks: BinaryHeap<Order>,
-    bids: BinaryHeap<Order>
+    bids: BinaryHeap<Order>,
 }
 
 impl Orderbook {
@@ -29,54 +29,30 @@ impl Orderbook {
         let mut executed_trades: Vec<Trade> = Vec::new();
 
         while !self.asks.is_empty() && !self.bids.is_empty() {
-            let (ask, bid) = (self.asks.pop().unwrap(), self.bids.pop().unwrap());
+            let (ask, bid) = (self.asks.peek().unwrap(), self.bids.peek().unwrap());
             // Match the optional of the merge
             match self.merge_orders(ask, bid) {
                 None => break,
                 Some((trade, remainder)) => {
+                    self.asks.pop();
+                    self.bids.pop();
                     executed_trades.push(trade);
                     if let Some(rem) = remainder {
                         // Apply the remainder to the order book
                         // iff the order has only been partially filled
                         match rem.side {
-                            Side::BUY => self.bids.push(rem.clone()),
-                            Side::SELL => self.asks.push(rem.clone()),
+                            Side::BUY => self.bids.push(rem),
+                            Side::SELL => self.asks.push(rem),
                         };
                     }
                 }
             }
         }
-        return executed_trades;
-    }
-
-    pub fn check_for_trades_stack_based_results(&mut self) -> Vec<Trade> {
-        let mut executed_trades: Vec<Trade> = Vec::new();
-
-        while !self.asks.is_empty() && !self.bids.is_empty() {
-            let (ask, bid) = (self.asks.pop().unwrap(), self.bids.pop().unwrap());
-            // Match the optional of the merge
-            match self.merge_orders(ask, bid) {
-                None => break,
-                Some((trade, remainder)) => {
-                    executed_trades.push(trade);
-                    if let Some(rem) = remainder {
-                        // Apply the remainder to the order book
-                        // iff the order has only been partially filled
-
-                        match rem.side {
-                            Side::BUY => self.bids.push(rem.clone()),
-                            Side::SELL => self.asks.push(rem.clone()),
-                        };
-                    }
-                }
-            }
-        }
-
         return executed_trades;
     }
 
     // Merge and return trade, also return any remaining orders that need filled
-    fn merge_orders(&self, ask: Order, bid: Order) -> Option<(Trade, Option<Order>)> {
+    fn merge_orders(&self, ask: &Order, bid: &Order) -> Option<(Trade, Option<Order>)> {
         let (ask, bid) = match (ask.side, bid.side) {
             (Side::BUY, Side::SELL) => (bid, ask),
             (Side::SELL, Side::BUY) => (ask, bid),
@@ -90,23 +66,34 @@ impl Orderbook {
         match ask.quantity.cmp(&bid.quantity) {
             Ordering::Equal => {
                 let quantity = ask.quantity;
-                Some((Trade { filled_quantity: quantity, ask, bid }, None))
+                Some((Trade { filled_quantity: quantity, ask: ask.clone(), bid: bid.clone() }, None))
             }
             Ordering::Greater => {
                 let quantity = bid.quantity;
                 let mut remainder = ask.clone();
                 remainder.quantity -= quantity;
-                Some((Trade { filled_quantity: quantity, ask, bid }, Some(remainder)))
+                Some((Trade { filled_quantity: quantity, ask: ask.clone(), bid: bid.clone() }, Some(remainder)))
             }
             Ordering::Less => {
                 let quantity = ask.quantity;
                 let mut remainder = bid.clone();
                 remainder.quantity -= quantity;
-                Some((Trade { filled_quantity: quantity, ask, bid }, Some(remainder)))
+                Some((Trade { filled_quantity: quantity, ask: ask.clone(), bid: bid.clone() }, Some(remainder)))
             }
         }
-
     }
+}
 
-
+impl Debug for Orderbook {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "-------------------Orderbook-------------------").unwrap();
+        for bid in &self.bids {
+            writeln!(f, "{0: <10} | {1: <10} | {2: <10} | {3: <10}", bid.client_id, "BUY", bid.quantity, bid.price).unwrap();
+        }
+        writeln!(f, "-----------------------------------------------").unwrap();
+        for ask in &self.asks {
+            writeln!(f, "{0: <10} | {1: <10} | {2: <10} | {3: <10}", ask.client_id, "SELL", ask.quantity, ask.price).unwrap();
+        }
+        writeln!(f, "-----------------Orderbook End-----------------")
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,28 +7,13 @@ use crate::engine::types::Side::{BUY, SELL};
 mod engine;
 
 fn main() {
-    let mut rng = rand::thread_rng();
     let mut orderbook = Orderbook::new();
 
-    for _ in 0..10000 {
-        let user_id = rng.gen_range(1000..100000);
-        let price = rng.gen_range(1..50);
-        let quantity = rng.gen_range(1..50);
-        let buy_order = Order::new(user_id, 1, price, quantity, BUY);
-
-        let user_id = rng.gen_range(1000..100000);
-        let price = rng.gen_range(1..50);
-        let quantity = rng.gen_range(1..50);
-        let sell_order = Order::new(user_id, 1, price, quantity, SELL);
-
-        orderbook.apply_order(buy_order);
-        orderbook.apply_order(sell_order);
-    }
-
-    // let buy_order = Order::new(001, 1, 100, 2, BUY);
-    // let sell_order = Order::new(002, 1, 100, 10, SELL);
-    // orderbook.apply_order(buy_order);
-    // orderbook.apply_order(sell_order);
+    println!("---------------1st Cycle with [BUY-2@100, SELL-10@101] orders added---------------");
+    let buy_order = Order::new(001, 1, 100, 2, BUY);
+    let sell_order = Order::new(002, 1, 101, 10, SELL);
+    orderbook.apply_order(buy_order);
+    orderbook.apply_order(sell_order);
 
     let match_cycle_start_time = Instant::now();
     let trades = orderbook.check_for_trades();
@@ -36,4 +21,21 @@ fn main() {
     let match_cycle_duration_micros =  match_cycle_start_time.elapsed().as_micros();
     println!("Matched Trades: {}", trades.len());
     println!("Matching cycle duration: {} microseconds", match_cycle_duration_micros);
+
+    println!("{:?}", orderbook);
+
+    println!("---------------2nd Cycle with BUY-4@101 order added---------------");
+    let matching_buy_for_sell_order = Order::new(003, 1, 101, 4, BUY);
+    orderbook.apply_order(matching_buy_for_sell_order);
+
+    println!("{:?}", orderbook);
+
+    let match_cycle_start_time = Instant::now();
+    let trades = orderbook.check_for_trades();
+
+    let match_cycle_duration_micros =  match_cycle_start_time.elapsed().as_micros();
+    println!("Matched Trades: {}", trades.len());
+    println!("Matching cycle duration: {} microseconds", match_cycle_duration_micros);
+
+    println!("{:?}", orderbook);
 }
