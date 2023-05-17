2d25fcfe18032e4e3f519bba5340ea9aa31444b9
Robert <rcgray306@gmail.com>
2023-05-17 23:44:27 +0100

    formatting added tests back

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use std::{net::SocketAddr, fs::read};
-
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::{TcpListener, TcpStream},
-    sync::broadcast,
 };
 
 mod engine;
@@ -33,17 +30,74 @@ async fn process(mut tcp_stream: TcpStream) {
     let mut reader = BufReader::new(reader);
     let mut line = String::new();
 
-    while let Ok(bytes_read_successfully) = reader.read_line(&mut line).await {
-        if bytes_read_successfully == 0 {
+    while let Ok(bytes_read_from_stream) = reader.read_line(&mut line).await {
+        if bytes_read_from_stream == 0 {
             break;
         }
 
-        println!("Read input of: {bytes_read_successfully} bytes");
         println!("Input: {:?}", line.trim().as_bytes());
 
         line.clear();
-        writer.write_all("Response\n".to_owned().as_bytes()).await.unwrap();  
+        writer
+            .write_all("Response\n".to_owned().as_bytes())
+            .await
+            .unwrap();
     }
 
     println!("Connection closed!");
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::engine::orderbook::Orderbook;
+    use crate::engine::types::Side::{BUY, SELL};
+    use crate::engine::types::{Order, Trade};
+    use std::time::Instant;
+
+    #[test]
+    fn order_book_test() {
+        let mut orderbook = Orderbook::new();
+        let mut executed_trades: Vec<Trade> = Vec::with_capacity(1000000);
+
+        use rand::Rng;
+        for _ in 1..10 {
+            let mut rng = rand::thread_rng();
+            let user_id = rng.gen_range(1000..100000);
+            let price = rng.gen_range(1..1000);
+            let quantity = rng.gen_range(1..50);
+            let buy_order = Order::new(user_id, 1, price, quantity, BUY);
+
+            let user_id = rng.gen_range(1000..100000);
+            let price = rng.gen_range(1..1000);
+            let quantity = rng.gen_range(1..50);
+            let sell_order = Order::new(user_id, 1, price, quantity, SELL);
+
+            orderbook.apply_order(buy_order);
+            orderbook.apply_order(sell_order);
+        }
+
+        let match_cycle_start_time = Instant::now();
+        orderbook.check_for_trades(&mut executed_trades);
+        let match_cycle_duration_micros = match_cycle_start_time.elapsed().as_micros();
+
+        println!(
+            "Matching cycle duration: {} microseconds",
+            match_cycle_duration_micros
+        );
+
+        println!("Matched Trades: {}", executed_trades.len());
+        println!("Trades:");
+        println!(
+            "{0: <3} | {1: <5} | {2: <5} | {3: <4}",
+            "Qty", "Px", "B/CId", "S/CId"
+        );
+
+        for trade in &executed_trades {
+            println!("{:?}", trade);
+        }
+
+        println!("{:?}", orderbook);
+
+        executed_trades.clear();
+    }
 }
