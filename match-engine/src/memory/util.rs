db4acf0d4030dfeceb184f730768ab6606f6117a
Robert <rcgray306@gmail.com>
2024-02-01 22:47:21 +0000

    Add inline always to memory fn

--- match-engine/src/memory/util.rs
+++ match-engine/src/memory/util.rs
@@ -1,5 +1,6 @@
 use std::mem::MaybeUninit;
 
+#[inline(always)]
 pub fn uninitialized_arr<T, const COUNT: usize>() -> [T; COUNT] {
     let buf: MaybeUninit<[T; COUNT]> = MaybeUninit::<[T; COUNT]>::uninit();
     let buffer = unsafe { buf.assume_init() };
