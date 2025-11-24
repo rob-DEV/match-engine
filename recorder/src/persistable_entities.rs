1e382ab8381d1d902aef9cdfbce06a1b1448ca84
Robert <masseymann@outlook.com>
2025-11-24 21:40:54 +0000

    Add engine persistence

--- recorder/src/persistable_entities.rs
+++ recorder/src/persistable_entities.rs
@@ -72,11 +72,10 @@ impl TradeRecord {
             instrument,
             exec_px, exec_qty, exec_type, exec_ns
         ) VALUES (
-            $1,$2,
-            $3,$4,$5,$6,
-            $7,$8,$9,$10,
-            $11,
-            $12,$13,$14,$15
+            $1,$2,$3,$4,$5,
+            $6,$7,$8,$9,
+            $10,
+            $11,$12,$13,$14
         )
         "#,
             t.trade_id,
